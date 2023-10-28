7675753c9ee3d01c482b60ed1ab76e913682409c
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-10-28 11:31:29 +0700

    [doc] added if error cant connect database

--- QuizPHP/connection.php
+++ QuizPHP/connection.php
@@ -1,3 +1,8 @@
 <?php
     $conn=mysqli_connect('localhost','root','','quiz');
+    // check if error cant connect in database
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
 ?>
